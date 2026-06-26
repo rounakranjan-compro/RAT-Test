af1a2fdef28bbbeb35a8caeb4e4bdfdc40245a51
sarthakgoyal19 <sarthak.goyal@comprotechnologies.com>
2026-06-26 13:12:47 +0530

    updated RunResultHeader

--- Frontend/src/components/RunResultHeader.vue
+++ Frontend/src/components/RunResultHeader.vue
@@ -11,7 +11,7 @@ defineProps({
 <template>
   <div
     class="rounded-t-xl 
-           border border-slate-800 px-6 py-5
+           border border-slate-900 px-6 py-5
            flex items-center justify-between bg-[#1c2333]
 "
   >
@@ -19,9 +19,9 @@ defineProps({
       <h2 class="text-xl font-semibold text-white">
         Latest Run Results
       </h2>
-      <p class="text-sm text-slate-400 mt-1">
+      <p class="text-sm text-slate-100 mt-1">
         Results for:
-        <span class="text-slate-300">
+        <span class="text-slate-200">
           {{title?.split('_').slice(0, -1).join('_') || title}}
         </span>
       </p>
